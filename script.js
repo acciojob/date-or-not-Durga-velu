e71f8154758d2815f47c567a5c4b46282ec652cc
Durga-velu <durgadevi134babu@gmail.com>
2025-03-05 20:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var isDate = function (input) {
   //   write your code here
 	let ans=new Date(input)
-	if(input.toString() !=="Invalid Date" && isNaN(input)){
+	if(input.toString() !=="Invalid Date" && !isNaN(input)){
 		return true
 	}
 	else{
